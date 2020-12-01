c87abe8068b66477cb669557ae08af6fb0c17b46
katsuren-kouta <katsurenkouta@gmail.com>
2020-12-01 23:02:51 +0900

    見出しコンポーネント作成

--- src/app/headline/headline.component.ts
+++ src/app/headline/headline.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadlineComponent implements OnInit {
 
+  rank:boolean;
   constructor() { }
 
   ngOnInit(): void {
+    this.rank = false;
   }
 
 }
